328551c16d5cdf885292ccd1d6f8ea62a32978b2
mrdarkstone99-code <mrdarkstone99@gmail.com>
2026-05-21 20:54:52 +0800

    Added WhatsApp style translator mode
    Added a new smart_translate function to handle chat-style translations.

--- utils/translator.py
+++ utils/translator.py
@@ -1,13 +1,48 @@
 from deep_translator import GoogleTranslator
 
-def translate_text(text, target="en"):
+
+def smart_translate(text, target="en", mode="chat"):
+    """
+    mode:
+    - formal = normal translation
+    - chat = WhatsApp style (simple + casual)
+    """
+
     try:
         translated = GoogleTranslator(
             source='auto',
             target=target
         ).translate(text)
 
+        if mode == "chat":
+            return make_chat_style(translated)
+
         return translated
 
-    except Exception:
+    except:
         return "Translation failed"
+
+
+def make_chat_style(text):
+    """
+    Converts formal text into WhatsApp style (simple learning mode)
+    """
+
+    rules = {
+        "I am": "I'm",
+        "I will": "I'll",
+        "do not": "don't",
+        "cannot": "can't",
+        "going to": "gonna",
+        "want to": "wanna",
+        "because": "coz",
+        "you are": "you're",
+        "please": "pls"
+    }
+
+    result = text
+
+    for k, v in rules.items():
+        result = result.replace(k, v)
+
+    return result
